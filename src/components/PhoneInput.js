2a47dba683417e9f760e260f15281e5bc2343743
JIHYE_PARK <qkrwlgp76@gmail.com>
2020-03-10 18:13:26 +0900

    number input event keyUp으로 변경

--- src/components/PhoneInput.js
+++ src/components/PhoneInput.js
@@ -4,12 +4,15 @@ import React, { useState } from 'react'
 const PhoneInput = () => {
   const [invalid, setInvalid] = useState(false)
   
-  const handleKeyPress = (ev) => {
+  const handleKeyUp = (ev) => {
     // '1234567890'.indexOf(ev.key) === -1 && ev.preventDefault()
 
-    const REGEX_NUMBER = /^[0-9\b]+$/
-    REGEX_NUMBER.test(ev.key) === false && ev.preventDefault() 
+    // const REGEX_NUMBER = /^[0-9\b]+$/
+    // REGEX_NUMBER.test(ev.key) === false && ev.preventDefault()
+
+    ev.target.value = ev.target.value.replace(/[^0-9\b]*/g, '') 
   }
+  
   const handleBlur = (ev) => {
     ev.target.value.length < 11 ? setInvalid(true) : setInvalid(false)
   }
@@ -25,7 +28,7 @@ const PhoneInput = () => {
       autoComplete="off" 
       maxLength="11"
       required 
-      onKeyPress={handleKeyPress}
+      onKeyUp={handleKeyUp}
       onBlur={handleBlur}
     />
     {invalid && <div className="invalid-feedback feedback">연락처 번호를 정확하게 입력해주세요.</div>}
